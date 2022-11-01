c3edacb7508e955f74a19ed5f09e9b08a8905342
Sayed Ali sharifian <62831406+S-Ali-S@users.noreply.github.com>
2022-11-01 21:19:03 +0330

    perf: refactor `reset` method to clear all collections (#14)

---
+++ tests/reset.test.ts
@@ -0,0 +1,23 @@
+import { Store } from '../src'
+import { schema, TypesMap } from './fixtures'
+import { postFactory, userFactory } from './utils/factories'
+import { toCollection } from './utils'
+
+const store = new Store<TypesMap>({
+  schema,
+})
+
+afterEach(() => store.reset())
+
+it('should clear all collections at once', () => {
+  toCollection(() => store.create('User', userFactory()), 5)
+  toCollection(() => store.create('Post', postFactory()), 5)
+
+  expect(store.count('User')).toEqual(5)
+  expect(store.count('Post')).toEqual(5)
+
+  store.reset()
+
+  expect(store.count('User')).toEqual(0)
+  expect(store.count('Post')).toEqual(0)
+})
