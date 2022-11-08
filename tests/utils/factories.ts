8ed22e7a08a76fbf27fb9943269eaeb16ce8e8a5
Mohammad Ataei <mammadataei@gmail.com>
2022-11-08 17:34:52 +0330

    chore(tests): update factories

--- tests/utils/factories.ts
+++ tests/utils/factories.ts
@@ -1,18 +1,37 @@
-import { Post, User, UserProfile } from '../fixtures'
+import { Comment, Post, User, Profile, Image, Text, Video } from '../fixtures'
 import {
   randUuid,
   randText,
   randUserName,
-  randParagraph,
-  randFullName,
+  randEmail,
   randUrl,
+  randNumber,
+  rand,
 } from '@ngneat/falso'
 
 export function postFactory(overrides?: Partial<Post>): Post {
   return {
     id: randUuid(),
-    title: randText({ charCount: 40 }),
-    body: randParagraph(),
+    author: userFactory(),
+    comments: {
+      count: 0,
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+      },
+    },
+    likes: 0,
+    likedBy: {
+      count: 0,
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+      },
+    },
+    content: rand([imageFactory, videoFactory, textFactory])(),
+    createdAt: new Date().toISOString(),
     ...overrides,
   }
 }
@@ -21,20 +40,80 @@ export function userFactory(overrides?: Partial<User>): User {
   return {
     id: randUuid(),
     username: randUserName(),
-    profile: userProfileFactory(),
-    posts: [],
+    email: randEmail(),
+    profile: profileFactory(),
     ...overrides,
   }
 }
 
-export function userProfileFactory(
-  overrides?: Partial<UserProfile>,
-): UserProfile {
+export function profileFactory(overrides?: Partial<Profile>): Profile {
   return {
-    fullName: randFullName(),
-    github: randUserName(),
-    twitter: randUserName(),
-    website: randUrl(),
+    username: randUserName(),
+    bio: randText(),
+    avatar: imageFactory(),
+    followers: {
+      count: 0,
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+      },
+    },
+    following: {
+      count: 0,
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+      },
+    },
+    posts: {
+      count: 0,
+      edges: [],
+      pageInfo: {
+        hasNextPage: false,
+        hasPreviousPage: false,
+      },
+    },
+    joinedAt: new Date().toISOString(),
+    ...overrides,
+  }
+}
+
+export function commentFactory(overrides?: Partial<Comment>): Comment {
+  return {
+    id: randUuid(),
+    author: userFactory(),
+    text: randText(),
+    createdAt: new Date().toString(),
+    ...overrides,
+  }
+}
+
+function imageFactory(overrides?: Partial<Image>): Image {
+  return {
+    url: randUrl(),
+    caption: randText(),
+    height: randNumber({ min: 100, max: 1024 }),
+    width: randNumber({ min: 100, max: 1024 }),
+    size: `${randNumber({ min: 100, max: 1024 })}kb`,
+    ...overrides,
+  }
+}
+
+function videoFactory(overrides?: Partial<Video>): Video {
+  return {
+    url: randUrl(),
+    caption: randText(),
+    duration: randNumber({ min: 100, max: 10 }),
+    mimeType: rand(['video/mp4', 'video/avi', 'video/mkv', 'video/webm']),
+    ...overrides,
+  }
+}
+
+function textFactory(overrides?: Partial<Text>): Text {
+  return {
+    text: randText(),
     ...overrides,
   }
 }
