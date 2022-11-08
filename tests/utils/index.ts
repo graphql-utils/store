516e782bb21b29a2737a983487861864ea79d503
Mohammad Ataei <mammadataei@gmail.com>
2022-11-08 19:03:54 +0330

    test: update and fix find.test.ts

--- tests/utils/index.ts
+++ tests/utils/index.ts
@@ -1,3 +1,9 @@
+import { PostContent, Text } from '../fixtures/schema.types'
+
 export function toCollection<T>(callback: () => T, times: number) {
   return Array.from({ length: times }, () => callback())
+}
+
+export function isTextContent(content: PostContent): content is Text {
+  return Reflect.has(content, 'text')
 }
